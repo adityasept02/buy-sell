9be07d795a1f7e855c15332698bc3d50d1f379ac
Aditya Kumar Maurya <aditya@Adityas-Laptop.local>
2024-08-09 11:04:21 +0530

    sas

---
+++ client/src/config.js
@@ -0,0 +1,21 @@
+export const API = "http://localhost:8000/api";
+// export const API = "https://casa-byte.vercel.app/api";
+export const FIREBASE_API_KEY = "AIzaSyBLeOj9DkNiuObcVa5Y3mJ3Q7O_6W32dsA";
+// export const FIREBASE_CONFIG = {
+//     apiKey: "AIzaSyDDK5O3i0wU-Ra54BEpxIJWlzhX_XV5kgU",
+//     authDomain: "realist-db192.firebaseapp.com",
+//     projectId: "realist-db192",
+//     storageBucket: "realist-db192.appspot.com",
+//     messagingSenderId: "941883088980",
+//     appId: "1:941883088980:web:1597f631e0f62b2b8d4dc1",
+//     measurementId: "G-5GW7D1WKJZ"
+//   };
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBLeOj9DkNiuObcVa5Y3mJ3Q7O_6W32dsA",
+    authDomain: "eal-estate.firebaseapp.com",
+    projectId: "eal-estate",
+    storageBucket: "eal-estate.appspot.com",
+    messagingSenderId: "260325061976",
+    appId: "1:260325061976:web:d32a47a983181e9f4c612d",
+    measurementId: "G-VMM78JRX1Y"
+  };
---
+++ server/config.js
@@ -0,0 +1,3 @@
+export const DATABASE = "mongodb+srv://aditya:aditya@buysell.3nbcfw8.mongodb.net/?retryWrites=true&w=majority&appName=buysell";
+export const JWT_SECRET = "FHJR2323RFWDFSFDS32S";
+export const FIREBASE_API_KEY = "AIzaSyDDK5O3i0wU-Ra54BEpxIJWlzhX_XV5kgU";
